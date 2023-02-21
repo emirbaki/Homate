8892c7d455827a813471cc57e9f1a4c72c8b791d
emirbaki <74327252+emirbaki@users.noreply.github.com>
2023-02-21 20:25:49 +0300

    dark tema switchi eklendi

---
+++ lib/widgets/theme_switch.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:homate/main.dart';
+import 'package:homate/utilities/color_utilities.dart';
+
+class ThemeSwitch extends StatefulWidget {
+  const ThemeSwitch({Key? key}) : super(key: key);
+
+  @override
+  State<ThemeSwitch> createState() => _ThemeSwitchState();
+}
+
+const List<Widget> icons = <Widget>[
+  Icon(Icons.dark_mode_rounded),
+  Icon(Icons.light_mode),
+];
+
+class _ThemeSwitchState extends State<ThemeSwitch> {
+  List<bool> modes = [false, true];
+  @override
+  Widget build(BuildContext context) {
+    return ToggleButtons(
+      direction: Axis.horizontal,
+      onPressed: (int index) {
+        setState(() {
+          // The button that is tapped is set to true, and the others to false.
+          for (int i = 0; i < modes.length; i++) {
+            modes[i] = i == index;
+            if (modes[i]) {
+              MyApp.of(context)
+                  .changeTheme(i == 0 ? ThemeMode.dark : ThemeMode.light);
+            }
+          }
+        });
+      },
+      borderRadius: const BorderRadius.all(Radius.circular(8)),
+      selectedBorderColor: HomateColorHelper.homateOrange[700],
+      selectedColor: Colors.white,
+      fillColor: HomateColorHelper.homateOrange[200],
+      color: HomateColorHelper.homateOrange[400],
+      constraints: const BoxConstraints(
+        minHeight: 40.0,
+        minWidth: 80.0,
+      ),
+      isSelected: modes,
+      children: icons,
+    );
+  }
+}
